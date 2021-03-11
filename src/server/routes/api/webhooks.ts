e0e5536fef6710374c22ee650d46ba38998dc38d
maanex <maan91315@gmail.com>
2021-03-11 12:31:48 +0100

    some debug logging

--- src/server/routes/api/webhooks.ts
+++ src/server/routes/api/webhooks.ts
@@ -14,17 +14,28 @@ router.post(
 
     const botid = req.body?.bot || undefined
     for (const project of await DatabaseManager.getProjects()) {
+      console.log('1')
       const conf = project.integrations?.topgg?.bots?.find(b => b.id === botid)
       if (!conf) continue
+      console.log('2')
 
-      if (auth !== conf.auth) return res.status(401).send('401 Unauthorized')
+      if (auth !== conf.auth) continue
+
+      console.log('3')
 
       const delta = (conf.reward_weekend !== undefined && req.body.isWeekend)
         ? conf.reward_weekend
         : conf.reward
 
+      if (req.body.type === 'test') {
+        console.log('TEST RUN')
+        continue
+      }
+
       const guild = await Core.guilds.fetch(project.discord_guild_id)
       if (!guild.members.resolve(req.body.user)) continue
+
+      console.log('4')
 
       const error = await DatabaseManager.modTokens(req.body.user, project._id, delta, {
         type: 'acquire',
@@ -32,6 +43,9 @@ router.post(
         timestamp: Date.now() / 1000,
         target: 'topgg'
       })
+
+      console.log('5')
+      console.log(error)
 
       if (!error && project.integrations.topgg.announce) {
         const settings = project.integrations.topgg.announce
@@ -46,8 +60,11 @@ router.post(
             : settings.text
           loc.send(text.split('{user}').join(`<@${req.body.user}>`))
         } catch (err) {
+          console.log('e2')
+          console.log(err)
         }
       }
+      console.log('6')
     }
 
     res.status(200).send('Thank you')
