67fc56a2f655e6ed84af13414fb1607e61e0d3ec
maanex <maan91315@gmail.com>
2021-04-08 18:09:00 +0200

    add config template

---
+++ config.template.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  server: {
+    port: 5008
+  },
+  bot: {
+    token: 'TOKEN',
+    clientid: '123123',
+    pubkey: 'abcabc'
+  },
+  mongodb: {
+    url: 'mongodb+srv://....',
+    dbname: 'greenlight'
+  },
+  admins: [
+    '137258778092503042'
+  ]
+}
